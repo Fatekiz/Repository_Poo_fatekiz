a97fbd5e6e362f92d68063d97419f20769f63e6a
Fatekiz <benjamin.concha.m10@gmail.com>
2024-10-23 10:39:22 -0300

    Obtener un atributo privado

--- Unidad_2/parcial/parcial1.1.py
+++ Unidad_2/parcial/parcial1.1.py
@@ -46,38 +46,56 @@ class Gato(): #Clase para la creaciones de los gatos
 
     def __str__(self): # para mostrar la información del gato
         return f"\n Información Completa y actual del gato:\n | Nombre: {self.__nombre} |\n | Color del gato: {self.__color} |\n | Color de ojos del gato: {self.__color_ojos} |\n | energia: {self.energia} |\n | hambre: {self.hambre} |\n  "
+    
+    def get_nombre(self):  
+        return self.__nombre        # <--- con este método publico podria acceder a los nombres aunque sean privados. (pero solo con esta manera) 
 
 
 class Area():
     def __init__(self,nombre,id,capacidad):
         self.__nombreArea = nombre
         self.__id = id
-        self.__capacidad = capacidad
+        self.__capacidad = capacidad              # <-- LISTA
         self.lista = []
     
-    # agregar gatos al area
+    # agregar gatos al areaA
     def agregar(self,gato):
         if len(self.lista) >= self.__capacidad:
-            print(f"\nel Area {self.__nombreArea} no tiene más capacidad para gatos, la capacidad total es de: {self.__capacidad}")
+            print(f"\n No se pudo ingresar el gato '{gato.get_nombre()}' | El Area '{self.__nombreArea}' no tiene más capacidad para gatos, la capacidad total es de: {self.__capacidad}\n")
         else:
             self.lista.append(gato)
-            print(f" ¡El gato con la información {gato} ha sido ingresado correctamente! Al área '{self.__nombreArea}'")
-            print(f" \nAhora la cantidad de gatos en el area es de {len(self.lista)}")
+            print(f"¡El gato {gato.get_nombre()} ha sido agregado a la lista exitosamente!")
+            print(f" Ahora la cantidad de gatos en el area '{self.__nombreArea}' es de {len(self.lista)}. (Recuerda que el máximo de gatos en este área es de {self.__capacidad}) \n")
 
     def mostrar_cantidad(self):
         if self.lista:
+            print(f"\n Gatos del área '{self.__nombreArea}' \n")      # <--- LISTO
             for i in self.lista:
-                print(f"\nInformaciones de todos los gatos presentes en el área {self.__nombreArea}\n") 
-                print(f" {i}")
+                print(i.get_nombre())
+        else:
+            print(f"No hay gatos en el Área '{self.__nombreArea}' ")
+            
+#Clase para los productos
+class Producto():
+    def __init__(self,nombre,cantidad):
+        self.nombre = nombre
+        self.cantidad = cantidad # inicial de cada producto
+    
 
+    #def usar(self,producto): # para usar cada producto 
+     #   if producto == "pelota":
+    
+       # elif producto == "salmon":
+        
 
+# clase para el inventario de productos
 class Inventario():
-    def __init__(self,alimentos,juguetes):
-        self.alimentos = alimentos
-        self.juguetes = juguetes
-        cantidadjuguetes = []
-
-
+    def __init__(self):
+        self.stock = [] # Lista vacia 
+    
+    
+    def agregar(self,producto):
+        self.stock.append(producto) 
 
 
 
@@ -93,9 +111,13 @@ gato3 = Gato("Cristian","amarillo","azules",75,75)
 gato4 = Gato("Liam","negro","Verdes",75,100)
 gato5 = Gato("Zayn","plomo","gris",99,75)
 
-#CREACIONES DE AREAS
+# CREACIONES DE AREAS
 area1 = Area("Recepcion",3354,2)
 area2 = Area("Comedor",3354,3)
+
+# CREANDO LOS PRODUCTOS
+pelota = Producto("pelota",2)
+salmon = Producto("salmon",1)
 
 
 
@@ -126,5 +148,8 @@ area2.agregar(gato3) # ahora si cristian xdd
 area2.agregar(gato4)
 area2.agregar(gato5)
 
+# MOSTRANDO LAS CANTIDADES DE GATOS EN CADA ARENA
 area1.mostrar_cantidad()
 area2.mostrar_cantidad()
+
+# ME FALTA AGREGAR LOS PRODUCTOS A SU RESPECTIVA LISTA.
