965ae5da5389c725f67d8a3e878deb78176b1cab
Fatekiz <benjamin.concha.m10@gmail.com>
2024-11-13 11:23:47 -0300

    Subo clase/actividad de herencia

---
+++ Unidad_3/herencia.py
@@ -0,0 +1,25 @@
+class Animal():
+    def __init__(self,nombre,edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def comer(self):
+        print(f"{self.name} está comiendo")
+
+    
+class Perro(Animal):
+    def __init__(self, nombre, edad,raza):
+        super().__init__(nombre, edad)
+        self.raza = raza
+    
+    def ladrar(self):
+        print(f"{self.nombre} está ladrando")
+
+class Gato(Animal):
+    def __init__(self, nombre, edad, color_pelaje):
+        super().__init__(nombre, edad)
+        self.color_pelaje = color_pelaje
+
+    def maullar(self):
+        print(f"{self.nombre} está maullando")
+
